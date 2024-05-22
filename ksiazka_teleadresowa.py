4ac8911544c66098df81bf8b804bb10635968a14
krzbaranagh <166720325+krzbaranagh@users.noreply.github.com>
2024-05-22 17:19:33 +0200

    Update ksiazka_teleadresowa.py
    stworzenie klasy

--- ksiazka_teleadresowa.py
+++ ksiazka_teleadresowa.py
@@ -1,1 +1,8 @@
+import pickle
 
+class Kontakt:
+    def __init__(self, imie, nazwisko, telefon, email):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.telefon = telefon
+        self.email = email
