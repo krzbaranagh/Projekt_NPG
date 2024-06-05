403d648f468120bc701bbc472a709a0adabd7abf
Bilu2003 <168099291+Bilu2003@users.noreply.github.com>
2024-06-05 23:32:27 +0200

    Update ksiazka_teleadresowa.py

--- ksiazka_teleadresowa.py
+++ ksiazka_teleadresowa.py
@@ -15,3 +15,10 @@ def zapisz_do_pliku(self, nazwa_pliku):
         with open(nazwa_pliku, 'wb') as plik:
             pickle.dump(self.kontakty, plik)
         print("Kontakty zostały zapisane do pliku.")
+ def wczytaj_z_pliku(self, nazwa_pliku):
+        try:
+            with open(nazwa_pliku, 'rb') as plik:
+                self.kontakty = pickle.load(plik)
+            print("Kontakty zostały wczytane z pliku.")
+        except FileNotFoundError:
+            print("Plik nie istnieje.")
