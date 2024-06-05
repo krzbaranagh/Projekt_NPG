f4d3eb8edb3ff62b75b584f8c6f7aeec76ac8537
Kuba Borowiecki <kubaxxxx18@gmail.com>
2024-06-05 15:48:55 +0200

    changing the name of objects

--- ksiazka_teleadresowa.py
+++ ksiazka_teleadresowa.py
@@ -18,58 +18,58 @@ import pickle
 import customtkinter as ctk
 from customtkinter import *
 
-app = ctk.CTk()
+main_window = ctk.CTk()
 
-app.title("Phonebook")
-app.geometry("650x430")
+main_window.title("Phonebook")
+main_window.geometry("650x430")
 
 
 def add_contact():
 
 
-    def close_new_window1():
-        new_window1.destroy()
+    def close_add_contact_window():
+        add_contact_window.destroy()
 
-    new_window1=ctk.CTk()
-    new_window1.title("Dodaj kontakt")
-    new_window1.geometry("400x400")
+    add_contact_window=ctk.CTk()
+    add_contact_window.title("Dodaj kontakt")
+    add_contact_window.geometry("400x400")
 
-    label_start = ctk.CTkLabel(new_window1, text="Podaj dane poniżej")
+    label_start = ctk.CTkLabel(add_contact_window, text="Podaj dane poniżej")
     label_start.pack()
 
-    label1 = ctk.CTkLabel(new_window1, text="Imię:")
-    label1.place(x=60, y=70)
+    name_label = ctk.CTkLabel(add_contact_window, text="Imię:")
+    name_label.place(x=60, y=70)
 
-    label2 = ctk.CTkLabel(new_window1, text="Nazwisko:")
-    label2.place(x=60, y=130)
+    surname_label = ctk.CTkLabel(add_contact_window, text="Nazwisko:")
+    surname_label.place(x=60, y=130)
 
-    label3 = ctk.CTkLabel(new_window1, text="Numer tel:")
-    label3.place(x=60, y=190)
+    phone_label = ctk.CTkLabel(add_contact_window, text="Numer tel:")
+    phone_label.place(x=60, y=190)
 
-    label4 = ctk.CTkLabel(new_window1, text="Email:")
-    label4.place(x=60, y=250)
+    email_label = ctk.CTkLabel(add_contact_window, text="Email:")
+    email_label.place(x=60, y=250)
 
 
-    entry1=CTkEntry(master=new_window1, placeholder_text="Start typing...", width=200, text_color="#FFCC70")
-    entry1.place(x=130, y=70)
+    name_entry=CTkEntry(master=add_contact_window, placeholder_text="Start typing...", width=200, text_color="#FFCC70")
+    name_entry.place(x=130, y=70)
 
-    entry2=CTkEntry(master=new_window1, placeholder_text="Start typing...", width=200, text_color="#FFCC70")
-    entry2.place(x=130, y=130)
+    surname_entry=CTkEntry(master=add_contact_window, placeholder_text="Start typing...", width=200, text_color="#FFCC70")
+    surname_entry.place(x=130, y=130)
 
-    entry3=CTkEntry(master=new_window1, placeholder_text="Start typing...", width=200, text_color="#FFCC70")
-    entry3.place(x=130, y=190)
+    phone_entry=CTkEntry(master=add_contact_window, placeholder_text="Start typing...", width=200, text_color="#FFCC70")
+    phone_entry.place(x=130, y=190)
 
-    entry4=CTkEntry(master=new_window1, placeholder_text="Start typing...", width=200, text_color="#FFCC70")
-    entry4.place(x=130, y=250)
+    email_entry=CTkEntry(master=add_contact_window, placeholder_text="Start typing...", width=200, text_color="#FFCC70")
+    email_entry.place(x=130, y=250)
 
-    button_confirm=CTkButton(master=new_window1, text="Zatwierdź", command=close_window_and_confirm_data, corner_radius=12)
+    button_confirm=CTkButton(master=add_contact_window, text="Zatwierdź", command=close_window_and_confirm_data, corner_radius=12)
     button_confirm.place(relx=0.98, rely=0.98, anchor="se")
     
 
-    button_cancel=CTkButton(master=new_window1, text="Anuluj", command=close_new_window1, corner_radius=12)
+    button_cancel=CTkButton(master=add_contact_window, text="Anuluj", command=close_add_contact_window, corner_radius=12)
     button_cancel.place(relx=0.02, rely=0.98, anchor="sw")
 
-    new_window1.mainloop()
+    add_contact_window.mainloop()
 
     
 
@@ -78,21 +78,23 @@ def add_contact():
 
 def display_contact():
     print("Przycisk 2 został naciśnięty")
-    app2=ctk.CTk()
-    app2.title("Display contact")
-    app2.geometry("600x400")
+    main_window2=ctk.CTk()
+    main_window2.title("Display contact")
+    main_window2.geometry("600x400")
+    #main_window2.mainloop()
+
 
 def edit_contact():
     print("Przycisk 3 został naciśnięty")
-    app3=ctk.CTk()
-    app3.title("Edit contact")
-    app3.geometry("600x400")
+    main_window3=ctk.CTk()
+    main_window3.title("Edit contact")
+    main_window3.geometry("600x400")
 
 def delete_contact():
     print("Przycisk 4 został naciśnięty")
-    app4=ctk.CTk()
-    app4.title("Delete contact")
-    app4.geometry("600x400")
+    main_window4=ctk.CTk()
+    main_window4.title("Delete contact")
+    main_window4.geometry("600x400")
 
 def save_to_file():
     print("ok")
@@ -101,8 +103,8 @@ def load_from_file():
     print("ok")
 
 
-def close_app():
-    app.destroy()
+def close_main_window():
+    main_window.destroy()
 
 
 
@@ -115,32 +117,32 @@ def close_window_and_confirm_data():
 
 
     
-button_close = ctk.CTkButton(master=app, text="Zamknij", command=close_app, corner_radius=12, fg_color="#a51b0b")
+button_close = ctk.CTkButton(master=main_window, text="Zamknij", command=close_main_window, corner_radius=12, fg_color="#a51b0b")
 button_close.place(relx=1, rely=0, anchor="ne")
 
-button1 = ctk.CTkButton(master=app, text="Dodaj kontakt", command=add_contact, width=200, height=50, corner_radius=12)
+button1 = ctk.CTkButton(master=main_window, text="Dodaj kontakt", command=add_contact, width=200, height=50, corner_radius=12)
 button1.pack(pady=10)
 
-button2 = ctk.CTkButton(master=app, text="Wyświetl dane kontaktu", command=display_contact, width=200, height=50, corner_radius=12)
+button2 = ctk.CTkButton(master=main_window, text="Wyświetl dane kontaktu", command=display_contact, width=200, height=50, corner_radius=12)
 button2.pack(pady=10)
 
-button3 = ctk.CTkButton(master=app, text="Edytuj kontakt", command=edit_contact, width=200, height=50, corner_radius=12)
+button3 = ctk.CTkButton(master=main_window, text="Edytuj kontakt", command=edit_contact, width=200, height=50, corner_radius=12)
 button3.pack(pady=10)
 
-button4 = ctk.CTkButton(master=app, text="Usuń kontakt", command=delete_contact, width=200, height=50, corner_radius=12)
+button4 = ctk.CTkButton(master=main_window, text="Usuń kontakt", command=delete_contact, width=200, height=50, corner_radius=12)
 button4.pack(pady=10)
 
-button5 = ctk.CTkButton(master=app, text="Zapisz dane do pliku", command=save_to_file, width=200, height=50, corner_radius=12)
+button5 = ctk.CTkButton(master=main_window, text="Zapisz dane do pliku", command=save_to_file, width=200, height=50, corner_radius=12)
 button5.pack(pady=10)
 
-button6 = ctk.CTkButton(master=app, text="Odczytaj dane z pliku", command=load_from_file, width=200, height=50, corner_radius=12)
+button6 = ctk.CTkButton(master=main_window, text="Odczytaj dane z pliku", command=load_from_file, width=200, height=50, corner_radius=12)
 button6.pack(pady=10)
 
-switch1=CTkSwitch(master=app, text="Tryb dla daltonistów")
+switch1=CTkSwitch(master=main_window, text="Tryb dla daltonistów")
 switch1.place(relx=0.98, rely=0.98, anchor="se")
 
 
     
 
 
-app.mainloop()
+main_window.mainloop()
