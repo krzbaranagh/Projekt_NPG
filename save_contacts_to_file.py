e1a8e0dcf9e4050319c5a98e95bbc40abbaa08bd
krzbaranagh <166720325+krzbaranagh@users.noreply.github.com>
2024-06-07 14:48:06 +0200

    Rozwinięcie funkcji

--- save_contacts_to_file.py
+++ save_contacts_to_file.py
@@ -6,3 +6,5 @@ from customtkinter import *
 import data_contact
 
 def save_contacts_to_file(ksiazka):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_dir, 'kontakty.txt')
