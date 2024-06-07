e500304946f9a6d46eb1a8ac4dda6c0b7a80cec6
krzbaranagh <166720325+krzbaranagh@users.noreply.github.com>
2024-06-07 14:48:50 +0200

    Update load_contacts_from_file.py

--- load_contacts_from_file.py
+++ load_contacts_from_file.py
@@ -6,3 +6,5 @@ from customtkinter import *
 import data_contact
 
 def load_contacts_from_txt(ksiazka):
+    current_dir = os.path.dirname(os.path.abspath(__file__)) 
+    file_path = os.path.join(current_dir, 'kontakty.txt')
