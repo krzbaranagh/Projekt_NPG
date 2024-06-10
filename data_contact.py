6b3c6481dd143003031714516856c08b3ff3b2c5
Taboredeq <166722448+Taboredeq@users.noreply.github.com>
2024-06-10 23:50:28 +0200

    Update data_contact.py
    tutaj zrobilem obiekt ksiazka bo jest ona wykozystywana w pliku display_data_contact

--- data_contact.py
+++ data_contact.py
@@ -9,6 +9,12 @@ class KsiazkaTeleadresowa:
     def __init__(self): 
         self.kontakty = []
 
+#k1 = Kontakt("Tomek", "Jajko",  543654564, "tomekjajko@gmail.com")
+#k2 = Kontakt("Jacek", "Placek", 769474839, "placekjacek@gmail.com")
+#k3 = Kontakt("Bogdan", "Doniek", 203633768, "DoniekB@gmail.com")
+#k4 = Kontakt("Maciej", "Wozny", 948793675, "KoloMaciejow@gmail.com")
 
+Ksiazka = KsiazkaTeleadresowa() #ten obiekt uzylem w wyswietlaniu kontaktow
+#Ksiazka.kontakty = [k1,k2,k3,k4]
 
 #tutaj gdzies mają być przechowywane obiekty
