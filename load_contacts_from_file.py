ef2f2767d5b07a670c49f50c9d7010dfefa1269f
krzbaranagh <166720325+krzbaranagh@users.noreply.github.com>
2024-06-11 15:31:10 +0200

    Dodanie okna potwierdzienia

--- load_contacts_from_file.py
+++ load_contacts_from_file.py
@@ -3,7 +3,24 @@ import os
 import customtkinter as ctk
 from customtkinter import *
 
-import data_contact
+from data_contact import *
+
+def show_confirmation_window():
+
+    confirmation_window = ctk.CTk()
+    confirmation_window.geometry("300x100")
+    confirmation_window.title("Potwierdzenie")
+
+    label = ctk.CTkLabel(confirmation_window, text="Pomyślnie zapisano kontakty do pliku")
+    label.pack(pady=10)
+
+    def close_window():
+        confirmation_window.destroy()
+
+    ok_button = ctk.CTkButton(confirmation_window, text="OK", command=close_window)
+    ok_button.pack(pady=10)
+
+    confirmation_window.mainloop()
 
 def load_contacts_from_txt(ksiazka):
     current_dir = os.path.dirname(os.path.abspath(__file__)) #ustalanie bieżącego katalogu
