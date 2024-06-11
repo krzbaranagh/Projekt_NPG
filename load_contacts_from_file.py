cd1aaf99ffe7a6f0c60d80ddef12f5d96f0163c9
krzbaranagh <166720325+krzbaranagh@users.noreply.github.com>
2024-06-11 14:39:14 +0200

    Update load_contacts_from_file.py

--- load_contacts_from_file.py
+++ load_contacts_from_file.py
@@ -7,7 +7,7 @@ import data_contact
 
 def load_contacts_from_txt(ksiazka):
     current_dir = os.path.dirname(os.path.abspath(__file__)) #ustalanie bieżącego katalogu
-    file_path = os.path.join(current_dir, 'kontakty.txt')    #znajdowanie pliku z kontaktami
+    file_path = os.path.join(current_dir, 'contacts_to_load.txt')    #znajdowanie pliku z kontaktami
 
     if not os.path.exists(file_path):
         print(f"Plik {file_path} nie istnieje.")
