70edb61854a4ad5b20ee96d8dce499fa7cd37f5d
krzbaranagh <166720325+krzbaranagh@users.noreply.github.com>
2024-06-11 16:41:49 +0200

    Aktualizacja funkcji

--- save_contacts_to_file.py
+++ save_contacts_to_file.py
@@ -5,16 +5,17 @@ from customtkinter import *
 
 import data_contact
 
-def save_contacts_to_file(ksiazka):
-    current_dir = os.path.dirname(os.path.abspath(__file__)) #ustalanie bieżącego katalogu
-    file_path = os.path.join(current_dir, 'contacts_to_save.txt') #znajdowanie pliku do którego będą zapisywane kontakty
+def save_contacts_to_txt():
+
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_dir, 'saved_contacts.txt')
 
     if not os.path.exists(file_path):
-        print(f"Plik {file_path} nie istnieje.")
+        show_error_window() 
         return
-        
+
     with open(file_path, 'w') as plik:
-        for kontakt in ksiazka.kontakty:
-            plik.write(f"{kontakt.imie}, {kontakt.nazwisko}, {kontakt.telefon}, {kontakt.email}\n")
-    
-    print(f"Kontakty zapisane do pliku {file_path}")
+        for kontakt in Ksiazka.kontakty:
+            plik.write(f"{kontakt.imie} {kontakt.nazwisko} {kontakt.telefon} {kontakt.email}\n")
+
+    show_confirmation_window()
