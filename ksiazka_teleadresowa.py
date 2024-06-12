e70da95fc95e038ba9436624aba08517465b07dc
Bilu2003 <168099291+Bilu2003@users.noreply.github.com>
2024-06-12 23:33:15 +0200

    Update ksiazka_teleadresowa.py

--- ksiazka_teleadresowa.py
+++ ksiazka_teleadresowa.py
@@ -1,24 +1,44 @@
 import pickle
+import customtkinter as ctk
+from customtkinter import *
+from data_contact import *
 
-class Kontakt:
-    def __init__(self, imie, nazwisko, telefon, email):
-        self.imie = imie
-        self.nazwisko = nazwisko
-        self.telefon = telefon
-        self.email = email
-if imie=0:
-print "imie =0"
-class KsiazkaTeleadresowa:
-    def __init__(self):
-        self.kontakty = []
-def zapisz_do_pliku(self, nazwa_pliku):
-        with open(nazwa_pliku, 'wb') as plik:
-            pickle.dump(self.kontakty, plik)
-        print("Kontakty zostały zapisane do pliku.")
- def wczytaj_z_pliku(self, nazwa_pliku):
-        try:
-            with open(nazwa_pliku, 'rb') as plik:
-                self.kontakty = pickle.load(plik)
-            print("Kontakty zostały wczytane z pliku.")
-        except FileNotFoundError:
-            print("Plik nie istnieje.")
+def delete_contact():
+    obiekt : Kontakt
+    niepusta : bool = len(Ksiazka.kontakty) != 0
+
+    if niepusta: 
+        obiekt = Ksiazka.kontakty[0]
+        lista = [adres.imie + " " + adres.nazwisko for adres in Ksiazka.kontakty]
+    else:
+        lista = ["Brak kontaktów"]
+    def wybor_kontaktu(wybor):
+        nonlocal obiekt
+        obiekt = Ksiazka.kontakty[wybor]
+
+    def delete_and_close_window():
+        #wyjebanie kontaktu
+        del obiekt
+        delete_contact_window.destroy()
+        
+    delete_contact_window=ctk.CTk()
+    delete_contact_window.resizable(False, False)
+    delete_contact_window.title("Delete contact")
+    delete_contact_window.geometry("400x150")
+    
+
+
+    label_start = ctk.CTkLabel(delete_contact_window, text="Kogo dane chcesz zobaczyć ?")
+    label_start.pack(pady=10)
+
+    contact_choice=CTkComboBox(master=delete_contact_window, values=lista, command = wybor_kontaktu)
+    contact_choice.pack(pady=10)
+
+    button_confirm=CTkButton(master=delete_contact_window, text="Zatwierdź", corner_radius=12, command=delete_and_close_window)
+    button_confirm.place(relx=0.99, rely=0.96, anchor="se")
+
+
+
+
+
+    delete_contact_window.mainloop()
