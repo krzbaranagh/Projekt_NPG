b1c8e8e54230ea8d5d97f03b79ba0079ec9cd59a
Kuba Borowiecki <kubaxxxx18@gmail.com>
2024-06-13 00:19:59 +0200

    s

--- delete_cont_funk.py
+++ delete_cont_funk.py
@@ -1,16 +1,30 @@
 import pickle
 import customtkinter as ctk
 from customtkinter import *
-
+from data_contact import *
 
 def delete_contact():
+    obiekt : Kontakt
+    niepusta : bool = len(Ksiazka.kontakty) != 0
 
+    if niepusta: 
+        
+        lista = [adres.imie + " " + adres.nazwisko for adres in Ksiazka.kontakty]
+    else:
+        lista = ["Brak kontaktów"]
+    def wybor_kontaktu(wybor):
+        nonlocal obiekt
+        for x in Ksiazka.kontakty:
+            if x.imie + " " + x.nazwisko == wybor:
+                obiekt = x
+                print("wykonalo")
     def delete_and_close_window():
         #wyjebanie kontaktu
+        if obiekt in Ksiazka.kontakty:
+            print("jest")
+            Ksiazka.kontakty.remove(obiekt)
         delete_contact_window.destroy()
-
-
-
+        
     delete_contact_window=ctk.CTk()
     delete_contact_window.resizable(False, False)
     delete_contact_window.title("Delete contact")
@@ -21,7 +35,7 @@ def delete_contact():
     label_start = ctk.CTkLabel(delete_contact_window, text="Kogo dane chcesz zobaczyć ?")
     label_start.pack(pady=10)
 
-    contact_choice=CTkComboBox(master=delete_contact_window, values=["option 1", "option 2", "option 3"])
+    contact_choice=CTkComboBox(master=delete_contact_window, values=lista, command = wybor_kontaktu)
     contact_choice.pack(pady=10)
 
     button_confirm=CTkButton(master=delete_contact_window, text="Zatwierdź", corner_radius=12, command=delete_and_close_window)
@@ -29,6 +43,6 @@ def delete_contact():
 
 
 
-
+#d
 
     delete_contact_window.mainloop()
