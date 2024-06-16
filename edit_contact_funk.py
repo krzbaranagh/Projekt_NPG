fd28eeb08293e0f4baa25be8184e25adeb9dcff6
FilipB <fbachanek@student.agh.edu.pl>
2024-06-17 00:08:04 +0200

    Type hinting w edit_contact

--- edit_contact_funk.py
+++ edit_contact_funk.py
@@ -3,39 +3,29 @@ from customtkinter import *
 from data_contact import *
 import data_variable
 
-def edit_contact():
-
-
-    
+def edit_contact() -> None:
     obiekt : Kontakt
-    niepusta : bool = len(Ksiazka.kontakty) != 0
 
-    if niepusta: 
+    if len(Ksiazka.kontakty): 
         obiekt = Ksiazka.kontakty[0]
         slownik = {(adres.imie + " " + adres.nazwisko) :adres for adres in Ksiazka.kontakty}
         lista = [adres.imie + " " + adres.nazwisko for adres in Ksiazka.kontakty]
     else:
         lista = ["Brak kontaktów"]
 
-    def set_value(wybor): 
+    def set_value(wybor) -> None: 
         nonlocal obiekt
         obiekt = slownik[wybor]
 
-
-
-
-    def confirm_and_edit():
+    def confirm_and_edit() -> None:
         choose_contact_window.destroy()
 
-
-        def save_changes_and_close_window():
-            
+        def save_changes_and_close_window() -> None:
             name=name_entry.get()
             surname=surname_entry.get()
             phone=phone_entry.get()
             email=email_entry.get()
-
-            data={
+            data = {
                 'imie':name,
                 'nazwisko':surname,
                 'telefon':phone,
@@ -48,15 +38,10 @@ def edit_contact():
 
             edit_contact_window.destroy()
             
-            
-
-        
-
         edit_contact_window=ctk.CTk()
         edit_contact_window.resizable(False, False)
         edit_contact_window.title("Edit contact")
         edit_contact_window.geometry("400x400")
-
 
         label_start = ctk.CTkLabel(edit_contact_window, text="Podaj dane poniżej")
         label_start.pack()
@@ -72,7 +57,6 @@ def edit_contact():
 
         email_label = ctk.CTkLabel(edit_contact_window, text="Email:")
         email_label.place(x=60, y=250)
-
 
         name_entry=CTkEntry(master=edit_contact_window, placeholder_text=obiekt.imie, width=200, text_color="#FFCC70")
         name_entry.place(x=140, y=70)
@@ -96,18 +80,15 @@ def edit_contact():
 
         edit_contact_window.mainloop()
         
-
-
     choose_contact_window=ctk.CTk()
     choose_contact_window.resizable(False, False)
     choose_contact_window.title("Edit contact")
     choose_contact_window.geometry("400x150")
 
-
     label_start = ctk.CTkLabel(choose_contact_window, text="Który kontakt chcesz edytować ?")
     label_start.pack(pady=10)
 
-    contact_choice=CTkComboBox(master=choose_contact_window, values=lista, command=set_value if niepusta else None, width=150)
+    contact_choice=CTkComboBox(master=choose_contact_window, values=lista, command=set_value if len(Ksiazka.kontakty) else None, width=150)
     contact_choice.pack(pady=10)
 
     button_confirm=CTkButton(master=choose_contact_window, text="Zatwierdź", corner_radius=12, command=confirm_and_edit)
