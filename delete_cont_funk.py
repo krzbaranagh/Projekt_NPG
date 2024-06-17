5efcc50ba0e95e085062b1a85d2ab01a669edc76
FilipB <fbachanek@student.agh.edu.pl>
2024-06-17 18:19:16 +0200

    Final delete_contact_funk.py

--- delete_cont_funk.py
+++ delete_cont_funk.py
@@ -31,7 +31,7 @@ def delete_contact() -> None:
             
         delete_contact_window.destroy()
         
-    delete_contact_window=ctk.CTk()
+    delete_contact_window = ctk.CTk()
     delete_contact_window.resizable(False, False)
     delete_contact_window.title("Delete contact")
     delete_contact_window.geometry("400x150")
@@ -39,21 +39,21 @@ def delete_contact() -> None:
     label_start = ctk.CTkLabel(delete_contact_window, text="Który kontakt chcesz usunąć ?")
     label_start.pack(pady=10)
 
-    contact_choice=CTkComboBox(master=delete_contact_window, values=lista, command = wybor_kontaktu, width=150)
+    contact_choice = CTkComboBox(master = delete_contact_window, values = lista, command = wybor_kontaktu, width = 150)
     contact_choice.pack(pady=10)
 
-    button_confirm=CTkButton(master=delete_contact_window, text="Zatwierdź", corner_radius=12, command=delete_and_close_window)
-    button_confirm.place(relx=0.99, rely=0.96, anchor="se")
+    button_confirm=CTkButton(master = delete_contact_window, text = "Zatwierdź", corner_radius = 12, command = delete_and_close_window)
+    button_confirm.place(relx = 0.99, rely = 0.96, anchor = "se")
 
-    button_cancel=CTkButton(master=delete_contact_window, text="Anuluj", command=delete_contact_window.destroy, corner_radius=12)
-    button_cancel.place(relx=0.02, rely=0.98, anchor="sw")
+    button_cancel=CTkButton(master = delete_contact_window, text = "Anuluj", command = delete_contact_window.destroy, corner_radius = 12)
+    button_cancel.place(relx = 0.02, rely = 0.98, anchor = "sw")
 
     if data_variable.DaltonMode: 
-        button_confirm.configure(fg_color="#1f6aa5", hover_color="#144870")
-        button_cancel.configure(fg_color="#1f6aa5", hover_color="#144870")
+        button_confirm.configure(fg_color = "#1f6aa5", hover_color = "#144870")
+        button_cancel.configure(fg_color = "#1f6aa5", hover_color = "#144870")
 
     else: 
-        button_confirm.configure(fg_color="#FF4500",hover_color="#FF6347")
-        button_cancel.configure(fg_color="#FF4500",hover_color="#FF6347")
+        button_confirm.configure(fg_color = "#FF4500", hover_color = "#FF6347")
+        button_cancel.configure(fg_color = "#FF4500", hover_color = "#FF6347")
 
     delete_contact_window.mainloop()
